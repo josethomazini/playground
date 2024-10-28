8af499aa401a444a701044f37b88cec0f31d5a07
José Thomazini <jose.thomazini@gmail.com>
2024-10-27 21:26:08 -0300

    prod db

--- clone-tabnews/pages/api/v1/status/index.js
+++ clone-tabnews/pages/api/v1/status/index.js
@@ -1,10 +1,32 @@
 import query from "infra/database";
 
 export default async function StatusPage(request, response) {
-  const resp = await query("select 1 + 1 as sum;");
-  const soma = resp.rows[0].sum;
-  console.log(soma);
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseVersionResult = await query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await query("SHOW max_connections;");
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await query({
+    text: "SELECT count(*)::int from pg_stat_activity where datname = $1",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  const updatedAt = new Date().toISOString();
   response.status(200).json({
-    chave: `${soma}`,
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 }
