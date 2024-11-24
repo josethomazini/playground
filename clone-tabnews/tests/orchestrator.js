56623888eb822333066275ec3c5a0b858781f282
José Thomazini <jose.thomazini@gmail.com>
2024-11-24 20:35:49 -0300

    stable tests

--- clone-tabnews/tests/orchestrator.js
+++ clone-tabnews/tests/orchestrator.js
@@ -2,11 +2,17 @@ import retry from "async-retry";
 
 async function fetchStatusPage() {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  await response.json();
+  if (response.status !== 200) {
+    throw Error();
+  }
 }
 
 async function waitForWebServer() {
-  return retry(fetchStatusPage, { retries: 100 });
+  return retry(fetchStatusPage, {
+    retries: 100,
+    minTimeout: 200,
+    maxTimeout: 1000,
+  });
 }
 
 async function waitForAllServices() {
