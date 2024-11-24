75e6170e82cb8249ab21e1ac044563a436496a2a
José Thomazini <jose.thomazini@gmail.com>
2024-11-24 20:58:00 -0300

    fix error

--- clone-tabnews/tests/integration/api/v1/status/get.test.js
+++ clone-tabnews/tests/integration/api/v1/status/get.test.js
@@ -6,7 +6,7 @@ beforeAll(async () => {
 
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe("200");
+  expect(response.status).toBe(200);
 
   const responseBody = await response.json();
   expect(responseBody.updated_at).toBeDefined();
