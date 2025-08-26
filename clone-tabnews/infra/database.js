6f2b9aae1fd493df8d6d4eddf88834ea90aea67c
José Thomazini <jose.thomazini@gmail.com>
2025-08-25 23:39:20 -0300

    feat: adds migrations endpoints

--- clone-tabnews/infra/database.js
+++ clone-tabnews/infra/database.js
@@ -1,17 +1,9 @@
 const { Client } = require("pg");
 
 async function query(query) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    port: process.env.POSTGRES_PORT,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
-
+  let client;
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(query);
     return result;
   } catch (error) {
@@ -22,6 +14,20 @@ async function query(query) {
   }
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    port: process.env.POSTGRES_PORT,
+    ssl: process.env.NODE_ENV === "production" ? true : false,
+  });
+  await client.connect();
+  return client;
+}
+
 module.exports = {
   query,
+  getNewClient,
 };
